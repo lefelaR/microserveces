73f42af8b65dd56dd792d32e7e14612d84957066
Rakheoana <rakheoana@turati.co.za>
2024-01-22 21:00:06 +0200

    config database

---
+++ Rakheoana/Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using Rakheoana.Models;
+
+namespace Rakheoana.Data
+{
+
+    public class AppDbContext :  DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> opt) :base(opt)
+        {
+        
+        }
+
+            public DbSet<Platform> Platforms { get; set; }
+    }
+}
